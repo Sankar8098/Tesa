435b8b1d4ab008dde0a3663a664b74355ca4b7a3
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-07-31 23:19:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,5 +12,5 @@ DB_NAME = os.environ.get("DATABASE_NAME", "cphdlust")
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "publicearn.com")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "2b4e167841be8af6f1d3254f2fccd2bed5518b19")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 43200)) # Add time in seconds
-IS_VERIFY = os.environ.get("IS_VERIFY", "True")
+IS_VERIFY = os.environ.get("IS_VERIFY", "False")
 TUT_VID = os.environ.get("TUT_VID", "https://t.me/Premium_Projects_Detail/5") # shareus ka tut_vid he 
