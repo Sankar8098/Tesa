b12deb8cad89f78f6eefe580962cffa0d99ad63a
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-08-01 09:08:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ DB_NAME = os.environ.get("DATABASE_NAME", "cphdlust")
 # check my discription to help by using my refer link of shareus.io
 
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "publicearn.com")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "2b4e167841be8af6f1d3254f2fccd2bed5518b19")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "kingurl.in")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "4861284f4d6a00f70ac74ab391459417842f53fb")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 43200)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "False")
 TUT_VID = os.environ.get("TUT_VID", "https://t.me/Premium_Projects_Detail/5") # shareus ka tut_vid he 
